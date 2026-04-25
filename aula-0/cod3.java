92fe72b5bee1124ff707e3e3e192f4784d94b60f
Gabriel Gonçalves <gabreilgon123.321@gmail.com>
2026-04-25 09:47:21 -0300

    aula0 código 3

---
+++ aula-0/cod3.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		float preco_laranja = 2.5f, preco_uva = 5f, total;
+		total = preco_laranja + preco_uva;
+		System.out.print(preco_laranja);
+		System.out.print('+');
+		System.out.println(preco_uva);
+		System.out.println("O preço da laranja é: "+  preco_laranja + ". O preco da uva é: "+ preco_uva+ ". O total é: " + total);
+	}
+}
