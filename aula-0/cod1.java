995e1e244b111283ed6f064e19f9f1e9bd0edfc0
Gabriel Gonçalves <gabreilgon123.321@gmail.com>
2026-04-25 09:45:38 -0300

    código aula 0

---
+++ aula-0/cod1.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		char pri_nome = 'G';
+		int idade = 15;
+		boolean tomei_cafe = true;
+		System.out.print("Primeira letra do meu nome é: "+ pri_nome);
+	}
+}
